1c7bf08b1efabb30203da441830b4d58d6293a07
akbarsaladin36 <asong36@gmail.com>
2020-07-04 15:01:29 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//    return view('welcome');
+// });
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/', 'HomeController@show');
+Route::get('/register', 'AuthController@form');
+Route::post('/welcome', 'AuthController@welcome');
+
+Route::get('/master', function () {
+    return view('adminlte/master');
+});
+
+Route::get('/items', function () {
+    return view('items.index');
+});
+
+Route::get('/items/create', function () {
+    return view('items.create');
+});
+
+Route::get('/data-tables', function () {
+    return view('items.datatables');
+});
+
+
+Route::get('/tambahItem', 'ItemController@create');
+Route::post('/items', 'ItemController@store');
+
+Route::get('/pertanyaan', function () {
+    return view('pertanyaan.index');
+});
+
+// CRUD 1
+Route::get('/pertanyaan', 'PertanyaanController@index');
+Route::get('/pertanyaan/create', 'PertanyaanController@create');
+Route::post('/pertanyaan', 'PertanyaanController@store');
+
+Route::get('/jawaban', 'JawabanController@index');
+Route::get('/jawaban/{pertanyaan_id}', 'JawabanController@show');
+Route::post('/jawaban/{pertanyaan_id}', 'JawabanController@tambah');
+
+// CRUD 2
+
+Route::get('/pertanyaan/{id}/edit', 'PertanyaanController@edit');
+Route::put('/pertanyaan/{id}', 'PertanyaanController@update');
+Route::delete('/pertanyaan/{id}', 'PertanyaanController@destroy');
+
